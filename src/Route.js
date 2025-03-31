02d64e1074c9088238f3410ba348a41ce68c8d7d
Inbaraj <inbarajr@amazon.com>
2025-03-31 11:43:09 +0530

    Updated GoodnewsAI and Privacy Policy pages

--- src/Route.js
+++ src/Route.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import Tapnav from './commponent/common/Topnav'
-import Home from './page/landingpage/Home'
-import Landing from './page/landingpage/landing'
-import Human from './page/who we are/Human'
-import Mtaudits from './page/who we are/Mtaudits'
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Tapnav from './commponent/common/Topnav';
+import Home from './page/landingpage/Home';
+import Landing from './page/landingpage/landing';
+import Human from './page/who we are/Human';
+import Mtaudits from './page/who we are/Mtaudits';
+
+// ✅ Import the new pages
+import GoodnewsAI from './page/GoodnewsAI';
+import PrivacyPolicy from './page/PrivacyPolicy';
 
 const route = () => {
   return (
     <div>
-              <Tapnav></Tapnav>
+      <Tapnav />
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Landing/>}></Route>
-        <Route path='/aiservice' element={<Home/>}></Route>
-        <Route path='/human' element ={<Human/>}></Route>
-        <Route path='/mtaudits' element={<Mtaudits/>}></Route>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/aiservice' element={<Home />} />
+          <Route path='/human' element={<Human />} />
+          <Route path='/mtaudits' element={<Mtaudits />} />
+
+          {/* ✅ New Routes for GoodnewsAI and Privacy Policy */}
+          <Route path='/GoodnewsAI' element={<GoodnewsAI />} />
+          <Route path='/GoodnewsAI/privacy-policy' element={<PrivacyPolicy />} />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
-}
-export default route
+  );
+};
+
+export default route;
